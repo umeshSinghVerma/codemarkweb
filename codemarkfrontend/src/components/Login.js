d7c289af0291095b987b8300408edc2a5a7676d1
umeshSinghVerma <usv0308@gmail.com>
2023-04-22 23:50:23 +0530

    Added Google sign in support

--- codemarkfrontend/src/components/Login.js
+++ codemarkfrontend/src/components/Login.js
@@ -1,35 +1,81 @@
 import React from 'react'
+import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { app } from "../firebaseConfig.js";
 import './Login.css'
 export default function Login() {
-  return (
-    <>
-    <h3 className='login-head'>Codemark</h3>
-    <div className='login-page'>
-        <div className='login-side1 login-side'>
-            <form className='login'>
-                <h4>Login</h4>
-                <div>
-                    <label htmlFor='email'>Email:</label>
-                    <input type="email" class="input-field" placeholder='Email' name="email"/>
+    const auth = getAuth();
+    function loginfn() {
+        let email = document.getElementById("login-emailid").value;
+        let password = document.getElementById("login-passwordid").value;
+        console.log(email, password);
+        // console.log(email,password);
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                console.log(userCredential.user);
+                alert("The user is signed in successfully");
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(error.message)
+                // handlefunction();
+                // ..
+            });
+    }
+    const provider = new GoogleAuthProvider();
+    function loginGooglefn() {
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                const user = result.user;
+                console.log(user);
+                if (user.email == "vea0308@gmail.com") {
+                    alert("This is a teacher");
+                }
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.customData.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                console.log(error.message);
+                // ...
+            });
+    }
+    return (
+        <>
+            <h3 className='login-head'>Codemark</h3>
+            <div className='login-page'>
+                <div className='login-side1 login-side'>
+                    <div className='login'>
+                        <h4>Login</h4>
+                        <div>
+                            <label htmlFor='email'>Email:</label>
+                            <input type="email" id='login-emailid' class="input-field" placeholder='Email' name="email" />
+                        </div>
+                        <div>
+                            <label htmlFor='password' >Password:</label>
+                            <input type="password" id='login-passwordid' class="input-field" placeholder='Password' name="password" />
+                        </div>
+                        <div>
+                            <label htmlFor='cpassword'>Confirm Password:</label>
+                            <input type="password" class="input-field" placeholder='Confirm Password' name="cpassword" />
+                        </div>
+                        <button className="login-button" onClick={loginfn}>Login</button>
+                        <p className='login-option'>OR</p>
+                        <button className='login-google' onClick={loginGooglefn}>Continue with Google</button>
+                        <p className='login-signup'>You dont have an account set ? <a href="#">Sign up</a></p>
+                    </div>
                 </div>
-                <div>
-                    <label htmlFor='password'>Password:</label>
-                    <input type="password" class="input-field" placeholder='Password' name="password"/>
+                <div className='login-side login-side2'>
+                    <img src="https://img.freepik.com/free-vector/academic-excellence-illustration_24877-52355.jpg?w=2000" className='login-img' />
                 </div>
-                <div>
-                    <label htmlFor='cpassword'>Confirm Password:</label>
-                    <input type="password" class="input-field" placeholder='Confirm Password' name="cpassword"/>
-                </div>
-                <button className="login-button">Login</button>
-                <p className='login-option'>OR</p>
-                <button className='login-google'>Continue with Google</button>
-                <p className='login-signup'>You dont have an account set ? <a href="#">Sign up</a></p>
-            </form>            
-        </div>
-        <div className='login-side login-side2'>
-            <img src="https://img.freepik.com/free-vector/academic-excellence-illustration_24877-52355.jpg?w=2000" className='login-img' />
-        </div>
-    </div>
-    </>
-  )
+            </div>
+        </>
+    )
 }
--- codemarkfrontend/src/components/Signup.js
+++ codemarkfrontend/src/components/Signup.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { app } from "../firebaseConfig.js";
 import './Signup.css'
 export default function Signup() {
+    const auth = getAuth();
     function Head() {
         return (
             <h1>Create Account</h1>
@@ -8,77 +11,92 @@ export default function Signup() {
     }
     function Email() {
         return (
-            <label for="email">Email<input id="signup-email" type="email" placeholder="Email" name="email"/></label>
+            <label for="email">Email<input id="signup-email" type="email" placeholder="Email" name="email" /></label>
         );
     }
     function Name() {
         return (
-            <label for="name">Name<input id="signup-name" type="text" placeholder="Name" name="name"/></label>
+            <label for="name">Name<input id="signup-name" type="text" placeholder="Name" name="name" /></label>
         );
     }
     function College_name() {
         return (
-            <label for="college-name">College Name<input id="signup-college-name" type="text" placeholder="College Name" name="college-name"/></label>
+            <label for="college-name">College Name<input id="signup-college-name" type="text" placeholder="College Name" name="college-name" /></label>
         );
     }
     function Enrollment_number() {
         return (
-            <label for="enrollment-number">Enrollment number<input id="signup-enrollment-number" type="text" placeholder="Enrollment Number" name="enrollment-number"/></label>
+            <label for="enrollment-number">Enrollment number<input id="signup-enrollment-number" type="text" placeholder="Enrollment Number" name="enrollment-number" /></label>
         );
     }
     function Joining_year() {
         return (
-            <label for="joining-year">Joining Year<input id="signup-joining-year" type="number" placeholder="Joining Year" name="joining=year" min="2010" max="2030"/></label>
+            <label for="joining-year">Joining Year<input id="signup-joining-year" type="number" placeholder="Joining Year" name="joining=year" min="2010" max="2030" /></label>
         );
     }
     function Semester() {
         return (
             <label for="semester">Semester
-            <select id="signup-semester">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-                <option>6</option>
-                <option>7</option>
-                <option>8</option>
-            </select>
+                <select id="signup-semester">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                </select>
             </label>
         );
     }
     function Password() {
         return (
-            <label for="signup-password">Password<input id="signup-password" type="password" placeholder="Password" name="signup-password"/></label>
+            <label for="signup-password">Password<input id="signup-password" type="password" placeholder="Password" name="signup-password" /></label>
         );
     }
     function RePassword() {
         return (
-            <label for="signup-cpassword">Confirm Password<input id="signup-cpassword" type="password" placeholder="Re-enter Password" name="signup-cpassword"/></label>
+            <label for="signup-cpassword">Confirm Password<input id="signup-cpassword" type="password" placeholder="Re-enter Password" name="signup-cpassword" /></label>
         );
     }
-  return (
-    <>
-    <h2 className="signup-company">CodeMark</h2>
-    <div class="signup-main">
-        <div className="signup-side signup-side1">
-            <Head />
-            <form className="signup">
-                <Email />
-                <Name />
-                <College_name />
-                <Enrollment_number />
-                <Joining_year />
-                <Semester />
-                <Password />
-                <RePassword />
-                <button className="signup-button">Create Account</button>
-            </form>
-        </div>
-        <div className="signup-side signup-side2">
-            <img className="signup-img" src="https://img.freepik.com/free-vector/academic-excellence-illustration_24877-52355.jpg?w=2000"/>
-        </div>
-    </div>
-    </>
-  )
+    function SignUpfn(e) {
+        e.preventDefault();
+        let email = document.getElementById("signup-email").value;
+        let password = document.getElementById("signup-password").value;
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                console.log("User is successfully created")
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(error.message);
+            });
+    }
+    return (
+        <>
+            <h2 className="signup-company">CodeMark</h2>
+            <div className="signup-main">
+                <div className="signup-side signup-side1">
+                    <Head />
+                    <form className="signup">
+                        <Email />
+                        <Name />
+                        <College_name />
+                        <Enrollment_number />
+                        <Joining_year />
+                        <Semester />
+                        <Password />
+                        <RePassword />
+                        <button className="signup-button" onClick={SignUpfn}>Create Account</button>
+                    </form>
+                </div>
+                <div className="signup-side signup-side2">
+                    <img className="signup-img" src="https://img.freepik.com/free-vector/academic-excellence-illustration_24877-52355.jpg?w=2000" />
+                </div>
+            </div>
+        </>
+    )
 }
